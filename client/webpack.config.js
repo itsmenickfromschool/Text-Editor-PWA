7e85d1cf6f11b58d9d7f08a737c596c2dc250cc9
Nick Burr <nicholasstclairburr@gmail.com>
2023-11-17 09:02:28 -0700

    added plug ins

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -18,6 +18,15 @@ module.exports = () => {
       path: path.resolve(__dirname, 'dist'),
     },
     plugins: [
+      new HtmlWebpackPlugin({
+        template: './index.html',
+        title: 'Contact Cards'
+      }),
+
+      new InjectManifest({
+        swSrc: './src-sw.js',
+        swDest: 'src-sw.js',
+      }),
       
     ],
 
