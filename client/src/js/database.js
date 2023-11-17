41674dfb622f8266954496d253c15a579fc353ce
Nick Burr <nicholasstclairburr@gmail.com>
2023-11-17 12:50:50 -0700

    changed the get and put so their is only one object

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -17,19 +17,20 @@ export const putDb = async (content) => {console.error('putDb not implemented');
 const jateDb = await openDB('jate',1);
 const tx = jateDb.transaction('jate', 'readwrite')
 const store = tx.objectStore('jate')
-const request = store.add({content})
+const request = store.put({id: 1, content})
 const result = await request
 console.log('Data saved to the database', result);
 }
 
 
 // TODO: Add logic for a method that gets all the content from the database
-export const getDb = async () => {console.error('getDb not implemented');
+export const getDb = async () => {
+  console.error('getDb not implemented');
 console.log('GET from the database');
 const contactDb = await openDB('jate', 1);
 const tx = contactDb.transaction('jate', 'readonly');
 const store = tx.objectStore('jate');
-const request = store.getAll();
+const request = store.get(1);
 const result = await request;
   console.log('result.value', result);
   return result;
